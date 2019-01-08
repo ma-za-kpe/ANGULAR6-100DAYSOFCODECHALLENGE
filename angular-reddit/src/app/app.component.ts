6fe1adda815c06907ac12704b91a7f0768a3bec3
ma-za-kpe <makpalyy@gmail.com>
2019-01-08 18:29:44 +0200

    BUILD: built the app and very ready for deployment oon heroku

--- angular-reddit/src/app/app.component.ts
+++ angular-reddit/src/app/app.component.ts
@@ -8,10 +8,10 @@ import { Article } from "./article/article.model";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  artcles: Article[];
+  articles: Article[];
 
   constructor() {
-    this.artcles = [
+    this.articles = [
       new Article("Angular 2", "http://angular.io", 3),
       new Article("Fullstack", "http://fullstack.io", 2),
       new Article("Angular Homepage", "http://angular.io", 0),
@@ -20,6 +20,13 @@ export class AppComponent {
   }
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
     console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = "";
+    link.value = "";
     return false;
+  }
+
+  sortedArticles(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
   }
 }
