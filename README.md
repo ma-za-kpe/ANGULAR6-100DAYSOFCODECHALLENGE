f172e37350f2ec449091c53a805439dd98216051
ma-za-kpe <makpalyy@gmail.com>
2019-01-09 11:27:45 +0200

    REDDIT LINK: add link to the readme.md file

--- README.md
+++ README.md
@@ -1,18 +1,25 @@
 # ANGULAR6-100DAYSOFCODECHALLENGE
+
 ## 100DaysOfCode
-## AUTHOR  
+
+## AUTHOR
+
 - MAKU PAULINE MAZAKPE
+
 ## DESCRIPTION
+
 - These practice projects are part of the challenge and the all come from the " ng-book: The Complete Guide to Angular 6
-by Nate Murray,  Felipe Coury, Ari Lerner, Carlos Taborda ".
+  by Nate Murray, Felipe Coury, Ari Lerner, Carlos Taborda ".
 
 - All the heroku links to the projects are added below.
 
-### Run locally by 
- + clone the projects
- + npm install
- + ng serve --o
+### Run locally by
+
+- clone the projects
+- npm install
+- ng serve --o
 
 # PROJECTS
+
 1. ANGULAR HELLOWORLD PROJECT
-2. [ANGULAR REDDIT CLONE](https://www.google.com)
+2. [ANGULAR REDDIT CLONE](https://still-gorge-60346.herokuapp.com/)
--- angular-reddit/server.js
+++ angular-reddit/server.js
@@ -8,6 +8,23 @@ var CONTACTS_COLLECTION = "contacts";
 var app = express();
 app.use(bodyParser.json());
 
-// Create link to Angular build directory
-var distDir = __dirname + "/dist/";
-app.use(express.static(distDir));
+// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
+var db;
+
+// Connect to the database before starting the application server.
+mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/reddit", function (err, client) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  // Save database object from the callback for reuse.
+  db = client.db();
+  console.log("Database connection ready");
+
+  // Initialize the app.
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
+});
