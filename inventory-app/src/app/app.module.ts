a3b665ed5feff12b3d5364636bbbdc85373f0b9b
ma-za-kpe <makpalyy@gmail.com>
2019-01-18 13:43:20 +0200

    PRODUCT LIST: add product-list component that holds the individual rows

--- inventory-app/src/app/app.module.ts
+++ inventory-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsListComponent } from './products-list/products-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsListComponent
   ],
   imports: [
     BrowserModule,
